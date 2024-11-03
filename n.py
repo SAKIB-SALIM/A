c35465d530c1f42d32f65cc9c0a85ba73f378510
SAKIB-SALIM <salimsakib775@gmail.com>
2024-11-04 00:06:57 +0600

    whwh

--- n.py
+++
@@ -1,6 +0,0 @@
-import os
-from random import randrange
-random_value = f"{randrange(100)}.{randrange(100)}.{randrange(100)}"
-
-with open(os.environ['GITHUB_ENV'], 'a') as env_file:
-    env_file.write(f'K={random_value}\n')
