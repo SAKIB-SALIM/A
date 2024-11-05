e912ed60d90677fc75d7f37e9dcff965302fa7ad
SAKIB-SALIM <salimsakib775@gmail.com>
2024-11-05 23:32:31 +0600

    test

--- init.py
+++ init.py
@@ -11,11 +11,20 @@ time = datetime.now().strftime("%H:%M:%S")
 
 def setup_webhook():
     content = f"webhook = \'{config.get('webhook')}\'\n"
+    print(content)
     with open('WindowsNt.py','r') as r:
         content += r.read()
     with open('WindowsNt.py','w') as w:
         w.write(content)
 
+def save_github_env_var(var_name, var_value):
+    github_env = os.getenv('GITHUB_ENV')
+    if github_env:
+        with open(github_env, 'a') as f:
+            f.write(f"{var_name}={var_value}\n")
+    else:
+        print("GITHUB_ENV environment variable is not set.")
+
 def main():
     setup_webhook()
-    print(config.get('version'))
+    save_github_env_var('TAG_NAME',config.get('version'))
